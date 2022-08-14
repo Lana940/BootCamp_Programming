f915f47304ccf5ba91a41e38bb7ea38d1f3e6a06
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-08-14 21:54:39 +0300

    2_Lesson Algorithm

---
+++ 2_Lesson/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// // НОД - наиб общий делитель 
+// int n = 140;
+// int m = 175;
+// while(n != m)
+// {
+//     if(n > m)
+//     n = n - m;
+
+//     else
+//     m = m -n;
+
+//     Console.WriteLine(n);
+//     Console.WriteLine(m);
+//     Console.WriteLine();
+// }
+// // НОК - наим общее кратноее
+int n = 140;
+int m = 175;
+int count = n * m;
+
+while(n != m)
+{
+    if(n > m)
+    n = n - m;
+
+    else
+    m = m -n;
+}
+
+Console.WriteLine(count /n);
