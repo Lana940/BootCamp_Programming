4b25792365564f76f0ae4ed5094094ea5d4b526b
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-08-17 17:12:41 +0300

    sortirovka puzirkom

---
+++ 4_Lesson/example_2/Example_3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность (сдвиг - циклический) на |K| элементов вправо, если K – положительное и влево, если отрицательное
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int[] b = new int[n];
+for (int i = 0; i < n; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+int k = Convert.ToInt32(Console.ReadLine());
+k = k % n;
+if (k > 0)
+{
+for (int i = 0; i <= k; i++)
+    b[i] = a[n - k + i];
+for (int i = 0; i < n - k; i++)
+    b[k + i] = a[i];
+for (int i = 0; i < n; i++)
+    Console.Write(b[i] + " ");
+}
+else
+{
+k = -k;
+for (int i = 0; i < k; i++)
+    b[n - k + i] = a[i];
+for (int i = 0; i < n - k; i++)
+    b[i] = a[k + i];
+for (int i = 0; i < n; i++)
+    Console.Write(b[i] + " ");
+}
