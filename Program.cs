bd77ff0c2be3727ad5c4c5113e1eb7355033ade7
Lana940 <svetlana.andreyevna@gmail.com>
2022-08-25 11:57:14 +0300

    9L

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿namespace Server //пространство имен, для использования одних и тех же переменных в разных файлах
+﻿namespace Client //пространство имен, для использования одних и тех же переменных в разных файлах
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("это наш сервер");
-            OurServer server = new OurServer();
-
-        }
+            Console.WriteLine("это наш клиент");
+            OurClient ourClient = new OurClient();
+    
+    }
     }
 }
