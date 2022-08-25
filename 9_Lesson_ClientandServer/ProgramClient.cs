be5dc07c4c2c25b77aeb2895e4987d035b310c61
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-08-25 12:00:03 +0300

    9Lesson code

---
+++ 9_Lesson_ClientandServer/ProgramClient.cs
@@ -0,0 +1,12 @@
+﻿namespace Client //пространство имен, для использования одних и тех же переменных в разных файлах
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("это наш клиент");
+            OurClient ourClient = new OurClient();
+    
+    }
+    }
+}
