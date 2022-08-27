7464110b16e3b736f94f22fff1ff384a8c61024d
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-08-27 15:13:46 +0300

    extended sort

---
+++ 10_Lesson_SortingByCounting/CountingSort_Extended/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + offset + 1];
+
+
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+
+    return sortedArray;
+}
